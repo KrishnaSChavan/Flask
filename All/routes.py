05e78dc087b7af82c92c73065d86ed07954b80f7
Krishna <krishnachavan538@gmail.com>
2023-03-13 23:13:16 +0530

    111

--- All/routes.py
+++ All/routes.py
@@ -6,4 +6,13 @@ def index():
     return "Hello World!"
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("Homepage.html",title="Home")
+
+
+@app.route("/about")
+def about():
+    return render_template("Homepage.html",title="About")
+
+@app.route("/account")
+def account():
+    return render_template("Account_page.html",title="Account")
---
+++ All/static/main.js
@@ -0,0 +1,2 @@
+var changecolor = document.querySelector(".test");
+changecolor.style.backgroundcolor = "#00ac00";

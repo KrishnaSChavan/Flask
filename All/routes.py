fa5985a43c877dcebaaffc59cc6e52f2052b6003
Krishna <krishnachavan538@gmail.com>
2023-03-14 12:51:20 +0530

    111

--- All/routes.py
+++ All/routes.py
@@ -2,17 +2,15 @@ from flask import Flask,render_template
 from All import app
 
 @app.route("/")
-def index():
-    return "Hello World!"
 @app.route("/home")
 def home():
-    return render_template("Homepage.html",title="Home")
+    return render_template("Homepage.html",title="Home",NAME='Home')
 
 
 @app.route("/about")
 def about():
-    return render_template("Homepage.html",title="About")
+    return render_template("Homepage.html",title="About",NAME="About")
 
 @app.route("/account")
 def account():
-    return render_template("Account_page.html",title="Account")
+    return render_template("Account_page.html",title="Account",NAME="Account")

a96a0e6b935fc1af9059f7846a4dbc9d88020f6e
Krishna <krishnachavan538@gmail.com>
2023-04-02 00:35:01 +0530

    111

--- All/__init__.py
+++ All/__init__.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'veryverysecreatkeycantbeshared'
 # pip3 uninstall flask_sqlalchemy and pip3 install flask-sqlalchemy==2.5.1 if not able to create db
 #app.config['SQLALCHEMY_MIGRATE_REPO'] = 'sqlite:///database/memb.db'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/all.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresqi://username:password@hostname:port/databasename'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresqi://xyz:1234567890@localhost:5432/members'
 
 db = SQLAlchemy(app)
 from All import routes
